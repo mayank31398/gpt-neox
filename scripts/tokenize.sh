0e059aa45fd9df3551a09d95d30b284e127526e5
mayank31398 <mayank31398@gmail.com>
2023-05-04 20:20:59 +0530

    use HF tokenizer

--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -1,16 +1,9 @@
-# download tokenizer
-mkdir -p vocab
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json -O vocab/gpt2-vocab.json
-wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt -O vocab/gpt2-merges.txt
-
 # tokenize dataset
 mkdir -p data
 python tools/preprocess_data.py \
     --input data_raw/debug/mix_uspto.jsonl.zst \
     --output-prefix data/debug \
-    --tokenizer-type GPT2BPETokenizer \
-    --vocab vocab/gpt2-vocab.json \
-    --merge-file vocab/gpt2-merges.txt \
+    --tokenizer-type HFGPT2Tokenizer \
     --dataset-impl mmap \
     --append-eod \
     --workers 1
